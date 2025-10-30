30c891c8d5d6f630db37fe5b13bcc59cefb2d448
bryan <bryansgonzalez156@gmail.com>
2025-10-30 01:03:42 -0400

    correct blinking

--- main.c
+++ main.c
@@ -2,7 +2,10 @@
 
 #define PERIPHERAL_BLOCK2_BASE (0x40000000U)
 #define AHB1_BASE (PERIPHERAL_BLOCK2_BASE + 0x20000U)
+
+/* Peripheral Bases*/
 #define GPIOA_BASE (AHB1_BASE + 0x0U)
+#define GPIOB_BASE (AHB1_BASE + 0x400U)
 #define RCC_BASE (AHB1_BASE + 0x3800U)
 
 /* HAVE REGISTER VALUES BE VOLATILE */
@@ -10,33 +13,47 @@
 #define RCC_AHB1ENR_OFFSET (0x30U)
 #define RCC_AHB1ENR ((volatile uint32_t*) (RCC_BASE + RCC_AHB1ENR_OFFSET))
 #define RCC_AHB1ENR_GPIOAEN (0x00U)
+#define RCC_AHB1ENR_GPIOBEN (0x01U)
 
 /* PORT A PIN 5*/
 #define GPIO_MODER_OFFSET (0x00U)
+
 #define GPIOA_MODER ((volatile uint32_t*) (GPIOA_BASE + GPIO_MODER_OFFSET))
+#define GPIOB_MODER ((volatile uint32_t*) (GPIOB_BASE + GPIO_MODER_OFFSET))
+
+#define GPIO_MODER_MODER0 (0U)
 #define GPIO_MODER_MODER5 (10U)
 #define GPIO_ODR_OFFSET (0x14U)
+
 /* Set value of register */
 #define GPIOA_ODR ((volatile uint32_t*) (GPIOA_BASE + GPIO_ODR_OFFSET))
+#define GPIOB_ODR ((volatile uint32_t*) (GPIOB_BASE + GPIO_ODR_OFFSET))
 
-#define LED_PIN 5
+#define LED_PIN 0
 
 void _init(void) {}
 
 int main(void)
 {
-  *RCC_AHB1ENR |= (1 << RCC_AHB1ENR_GPIOAEN);
+  /* Enable Port A clock */
+  //*RCC_AHB1ENR |= (1 << RCC_AHB1ENR_GPIOAEN);
+
+  /* Enable PORTA clock */
+  *RCC_AHB1ENR |= (1 << RCC_AHB1ENR_GPIOBEN);
 
   // do two dummy reads after enabling the peripheral clock, as per the errata
   volatile uint32_t dummy;
   dummy = *(RCC_AHB1ENR);
   dummy = *(RCC_AHB1ENR);
 
-  *GPIOA_MODER |= (1 << GPIO_MODER_MODER5);
+  /* Mode of the GPIO Pin*/
+  //*GPIOA_MODER |= (1 << GPIO_MODER_MODER5);
+  *GPIOB_MODER |= (1 << GPIO_MODER_MODER0);
   
   while(1)
   {
-    *GPIOA_ODR ^= (1 << LED_PIN);
+    //*GPIOA_ODR ^= (1 << LED_PIN);
+    *GPIOB_ODR ^= (1 << LED_PIN);
     for (uint32_t i = 0; i < 1000000; i++);
   }
   return 0;
